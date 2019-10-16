3f89bd43a193a61262741eb9d6fb9fad8ad828f6
thinguyen-logivan <thina@logivan.com>
2019-10-16 13:47:53 +0700

    Complete setup and apply addon-jsx, addon-actions, addon-knobs, addon-notes and addon-viewport

---
+++ .storybook/addons.js
@@ -0,0 +1,29 @@
+/**
+ * Addon JSX shows you the JSX of the story
+ */
+
+import 'storybook-addon-jsx/register';
+
+/**
+ * Actions can be used to display data received by event handlers
+ */
+
+import '@storybook/addon-actions/register';
+
+/**
+ *  Knobs allow you to edit props dynamically using the Storybook UI
+ */
+
+import '@storybook/addon-knobs/register';
+
+/**
+ * Notes allows you to write notes (text or HTML)
+ */
+
+import '@storybook/addon-notes/register-panel';
+
+/**
+ * Viewport Addon allows your stories to be displayed in different sizes and layouts
+ */
+
+import '@storybook/addon-viewport/register';
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,60 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const extractCss = new MiniCssExtractPlugin({
+  filename: '[name].[hash:8].css',
+});
+
+module.exports = ({ config, mode }) => {
+  /**
+   * SASS Config
+   */
+
+  config.module.rules.push({
+    test: /\.s[ac]ss$/i,
+    sideEffects: false,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: mode === 'development',
+        },
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          localsConvention: 'camelCase',
+          modules: {
+            mode: 'local',
+            localIdentName: '[name]__[local]--[hash:base64:5]',
+          },
+        },
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          config: {
+            path: '.config/postcss.config.js',
+          },
+        },
+      },
+      {
+        loader: 'sass-loader',
+      },
+      {
+        loader: 'sass-resources-loader',
+        options: {
+          resources: 'src/styles/resource.scss',
+        },
+      },
+    ],
+  });
+
+  /**
+   * Add Plugins
+   */
+
+  config.plugins.push(extractCss);
+
+  return config;
+};
